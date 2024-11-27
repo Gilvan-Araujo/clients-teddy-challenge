473538a3d788158114204353b0aef2f0b44f5e66
Gilvan Araujo <gilvanjunior@protonmail.com>
2024-11-27 19:40:45 -0300

    feat(navigation): implement simple drawer in the application

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
+import 'react-native-gesture-handler';
+
 import {
   Inter_400Regular,
   Inter_700Bold,
   useFonts,
 } from '@expo-google-fonts/inter';
-import { ActivityIndicator, SafeAreaView } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { DrawerToggleButton } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { ActivityIndicator, Image, SafeAreaView } from 'react-native';
 
 import Greetings from './src/screens/Greetings';
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      id={undefined}
+      screenOptions={{
+        headerShown: true,
+        drawerPosition: 'right',
+        drawerStyle: { maxWidth: 280, marginRight: 0, right: 0, width: '100%' },
+        headerLeft: () => null,
+        headerRight: (props) => (
+          <DrawerToggleButton {...props} tintColor="#666666" />
+        ),
+        headerTitle: () => (
+          <Image
+            source={require('./assets/teddy-logo.png')}
+            style={{ maxWidth: 70, maxHeight: 35 }}
+          />
+        ),
+      }}
+      initialRouteName="Saudações"
+    >
+      <Drawer.Screen name="Saudações">
+        {() => (
+          <SafeAreaView style={{ flex: 1, padding: 20 }}>
+            <Greetings />
+          </SafeAreaView>
+        )}
+      </Drawer.Screen>
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -28,8 +67,8 @@ export default function App() {
   }
 
   return (
-    <SafeAreaView style={{ flex: 1, padding: 20 }}>
-      <Greetings />
-    </SafeAreaView>
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
   );
 }
