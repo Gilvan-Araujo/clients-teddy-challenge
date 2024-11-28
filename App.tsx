75233de450f63f1ef02f35f2f4472fd2981f9ba8
Gilvan Araujo <gilvanjunior@protonmail.com>
2024-11-28 08:59:24 -0300

    feat(navigation): abstract drawer navigator to separate component and add mockup screens

--- App.tsx
+++ App.tsx
@@ -5,46 +5,10 @@ import {
   Inter_700Bold,
   useFonts,
 } from '@expo-google-fonts/inter';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { DrawerToggleButton } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { ActivityIndicator, Image, SafeAreaView } from 'react-native';
+import { ActivityIndicator, SafeAreaView } from 'react-native';
 
-import Greetings from './src/screens/Greetings';
-
-const Drawer = createDrawerNavigator();
-
-function MyDrawer() {
-  return (
-    <Drawer.Navigator
-      id={undefined}
-      screenOptions={{
-        headerShown: true,
-        drawerPosition: 'right',
-        drawerStyle: { maxWidth: 280, marginRight: 0, right: 0, width: '100%' },
-        headerLeft: () => null,
-        headerRight: (props) => (
-          <DrawerToggleButton {...props} tintColor="#666666" />
-        ),
-        headerTitle: () => (
-          <Image
-            source={require('./assets/teddy-logo.png')}
-            style={{ maxWidth: 70, maxHeight: 35 }}
-          />
-        ),
-      }}
-      initialRouteName="Saudações"
-    >
-      <Drawer.Screen name="Saudações">
-        {() => (
-          <SafeAreaView style={{ flex: 1, padding: 20 }}>
-            <Greetings />
-          </SafeAreaView>
-        )}
-      </Drawer.Screen>
-    </Drawer.Navigator>
-  );
-}
+import Drawer from './src/components/Drawer';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -68,7 +32,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <MyDrawer />
+      <Drawer />
     </NavigationContainer>
   );
 }
---
+++ src/components/Drawer.tsx
@@ -0,0 +1,110 @@
+import {
+  DrawerToggleButton,
+  createDrawerNavigator,
+} from '@react-navigation/drawer';
+import React from 'react';
+import { Image, SafeAreaView } from 'react-native';
+
+import ClientsIcon from '../../assets/clients.svg';
+import HomeIcon from '../../assets/home.svg';
+import ProductsIcon from '../../assets/products.svg';
+import Clients from '../screens/Clients';
+import Greetings from '../screens/Greetings';
+import Home from '../screens/Home';
+import Products from '../screens/Products';
+
+const DrawerNavigator = createDrawerNavigator();
+
+export default function Drawer() {
+  return (
+    <DrawerNavigator.Navigator
+      id={undefined}
+      screenOptions={{
+        headerShown: true,
+        drawerPosition: 'right',
+        drawerStyle: { maxWidth: 280, marginRight: 0, right: 0, width: '100%' },
+        headerLeft: () => null,
+        headerRight: (props) => (
+          <DrawerToggleButton {...props} tintColor="#666666" />
+        ),
+        headerTitle: () => (
+          <Image
+            source={require('../../assets/teddy-logo.png')}
+            style={{ maxWidth: 70, maxHeight: 35 }}
+          />
+        ),
+
+        drawerActiveTintColor: '#EC6724',
+      }}
+      initialRouteName="greetings"
+    >
+      <DrawerNavigator.Screen
+        name="home"
+        options={{
+          title: 'Home',
+          headerShown: true,
+          drawerIcon: (props) => {
+            return <HomeIcon color={props.focused ? '#EC6724' : '#666666'} />;
+          },
+        }}
+      >
+        {() => (
+          <SafeAreaView style={{ flex: 1, padding: 20 }}>
+            <Home />
+          </SafeAreaView>
+        )}
+      </DrawerNavigator.Screen>
+
+      <DrawerNavigator.Screen
+        name="greetings"
+        options={{
+          headerShown: false,
+          drawerStyle: { display: 'none' },
+          drawerItemStyle: { display: 'none' },
+        }}
+      >
+        {() => (
+          <SafeAreaView style={{ flex: 1, padding: 20 }}>
+            <Greetings />
+          </SafeAreaView>
+        )}
+      </DrawerNavigator.Screen>
+
+      <DrawerNavigator.Screen
+        name="clients"
+        options={{
+          title: 'Clientes',
+          drawerIcon: (props) => {
+            return (
+              <ClientsIcon color={props.focused ? '#EC6724' : '#666666'} />
+            );
+          },
+        }}
+      >
+        {() => (
+          <SafeAreaView style={{ flex: 1, padding: 20 }}>
+            <Clients />
+          </SafeAreaView>
+        )}
+      </DrawerNavigator.Screen>
+
+      <DrawerNavigator.Screen
+        name="products"
+        options={{
+          title: 'Produtos',
+          drawerIcon: (props) => {
+            return (
+              <ProductsIcon color={props.focused ? '#EC6724' : '#666666'} />
+            );
+          },
+        }}
+      >
+        {() => (
+          <SafeAreaView style={{ flex: 1, padding: 20 }}>
+            <Products />
+          </SafeAreaView>
+        )}
+      </DrawerNavigator.Screen>
+    </DrawerNavigator.Navigator>
+  );
+}
